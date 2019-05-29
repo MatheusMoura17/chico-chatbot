9d2f54a58202e99acaf61db7b983f32d0e576e4a
Matheus Moura <matheus.moura13@gmail.com>
2019-05-28 22:10:14 -0300

    Adiciona componente LayoutDrawer na main app

--- pwa/src/App.tsx
+++ pwa/src/App.tsx
@@ -1,8 +1,16 @@
-import * as React from "react";
+import React, { Component } from "react";
+import { CssBaseline } from "@material-ui/core";
+
+import LayoutDrawer from "./components/LayoutDrawer";
 
 /** Aplicação principal */
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
-    return <div>Olá mundo!</div>;
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <LayoutDrawer title="Chicó ChatBot" />
+      </React.Fragment>
+    );
   }
 }
